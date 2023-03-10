6d0b5f30a070348ef6a3ec8f9de3c6fa076af1a4
PaulBenaroch <Paul.benaroch@eleves.ec-nantes.fr>
2023-03-10 11:03:28 +0100

    Update tortue.h

---
+++ tortue.h
@@ -0,0 +1,21 @@
+/**
+ * \file     .hpp
+ * \brief    Définition de la classe poinT
+ * \author   BENAROCH Paul
+ * \version  0.1
+ * \date     07/12/2022
+ *
+ */
+
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class tortue {
+    public:
+    string couleur;
+    int positionX; //abscisse 1->10
+    int positionY; //ordonnée 1->5
+    
+}
