5463090d08a68004a3877207093f31f822053459
Romain <101245889+Xquiratis@users.noreply.github.com>
2023-03-10 09:35:50 +0100

    starting

---
+++ carte.h
@@ -0,0 +1,16 @@
+class carte
+{
+private:
+    /* data */
+public:
+    carte(/* args */);
+    ~carte();
+};
+
+carte::carte(/* args */)
+{
+}
+
+carte::~carte()
+{
+}
---
+++ partie.h
@@ -0,0 +1,16 @@
+class partie
+{
+private:
+    /* data */
+public:
+    partie(/* args */);
+    ~partie();
+};
+
+partie::partie(/* args */)
+{
+}
+
+partie::~partie()
+{
+}
