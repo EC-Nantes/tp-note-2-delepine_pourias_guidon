d973206ef4c7fbd0f43f4403bf2c6d52e77d3aac
Romain <101245889+Xquiratis@users.noreply.github.com>
2023-03-15 15:03:34 +0100

    maj

--- carte.h
+++ carte.h
@@ -4,19 +4,6 @@
 #include "cartes.h"
 
 
-class carte
-  {
-  protected:
-    COULEUR couleurs;
-    ACTION actions;
-  public:
-    carte();
-    carte(COULEUR couleur);
-    virtual void action()=0;
-    int getCouleur(){return this->couleurs;};
-    int getAction(){return this->actions;};
-  };
-
 
 
   
