c123d684fc78cc32dd867d41337fbc54cb2caf72
PaulBenaroch <Paul.benaroch@eleves.ec-nantes.fr>
2023-03-15 14:47:05 +0100

    Nul

---
+++ cartes.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "partie.h"
+#include "joueur.h"
+#include "cartes.h"
---
+++ tortue.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "partie.h"
+#include "joueur.h"
+#include "cartes.h"
+
+/**
+ * \file     tortue.h
+ * \brief    Définition de la classe tortue
+ * \author   BENAROCH POURIAS GUIDON DELEPINE
+ * \version  0.1
+ * \date     10/03/2023
+ *
+ */
+
+#pragma once
+#include <vector>
+#include "partie.h"
+#include "joueur.h"
+#include "cartes.h"
+
+using namespace std;
+
+class tortue {
+    public:
+    COULEUR couleur;
+    int positionX; //abscisse 1->10
+    int positionY; //ordonnée 1->5
+
+    /*
+    ** Méthode: Constructeur de la classe
+    */
+    tortue(COULEUR couleur, int x, int y);
+    /*
+    ** Méthode: Seteur de la valeur X
+    */
+   void setCoordoX(int x);
+    /*
+    ** Méthode: Seteur de la valeur X
+    */
+   void setCoordoY(int y);
+    /*
+    ** Méthode: Accesseur à la valeur de X
+    */
+    int getCoordoX();
+    /*
+    ** Méthode: Accesseur à la valeur de y
+    */
+    int getCoordoY();
+    
+};
