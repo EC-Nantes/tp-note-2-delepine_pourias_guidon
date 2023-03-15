dd806f333de6c12bac6239d9dac6e18a43b9ef6f
Romain <101245889+Xquiratis@users.noreply.github.com>
2023-03-15 16:03:50 +0100

    plus de point h

--- tortue.h
+++ tortue.h
@@ -41,3 +41,49 @@ class tortue {
     int getCoordoY();
     
 };
+
+
+/*
+** Méthode: Constructeur de la classe
+*/
+tortue::tortue(COULEUR nouvCouleur, int x, int y){
+    this -> couleur= nouvCouleur;
+    this -> positionX= x;
+    this -> positionY= y;
+};
+
+/*
+    ** Méthode: Seteur de la valeur X
+    */
+void tortue::setCoordoX(int x)
+{
+    this -> positionX= x;
+}
+
+
+/*
+    ** Méthode: Seteur de la valeur Y
+    */
+void tortue::setCoordoY(int y)
+{
+    this -> positionY= y;
+}
+
+
+/*
+    ** Méthode: Accesseur à la valeur de X
+    */
+int tortue::getCoordoX()
+{
+    return this -> positionX ;
+}
+
+
+/*
+    ** Méthode: Accesseur à la valeur de Y
+    */
+int tortue::getCoordoY()
+{
+    return this -> positionY;
+}
+
