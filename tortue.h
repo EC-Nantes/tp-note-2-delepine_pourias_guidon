76f69f9f8920fe3ae2339e57cec709dd83a27467
RobinDELEPINE <robin.delepine1901@gmail.com>
2023-03-15 17:04:40 +0100

    Robin a fini d'ajouter ses modifications

--- tortue.h
+++ tortue.h
@@ -1,87 +1,41 @@
-/**
- * \file     tortue.h
- * \brief    Définition de la classe tortue
- * \author   BENAROCH POURIAS GUIDON DELEPINE
- * \version  0.1
- * \date     10/03/2023
- *
- */
-
-#pragma once
-#include "ENUM.h"
-#include <vector>
-
-using namespace std;
-
-class tortue {
-    public:
-    COULEUR couleur;
-    int positionX = 0; //abscisse 1->10
-    int positionY = 0; //ordonnée 1->5
-
-    /*
-    ** Méthode: Constructeur de la classe
-    */
-    tortue(COULEUR couleur);
-    /*
-    ** Méthode: Seteur de la valeur X
-    */
-   void setCoordoX(int x);
-    /*
-    ** Méthode: Seteur de la valeur X
-    */
-   void setCoordoY(int y);
-    /*
-    ** Méthode: Accesseur à la valeur de X
-    */
-    int getCoordoX();
-    /*
-    ** Méthode: Accesseur à la valeur de y
-    */
-    int getCoordoY();
-    
+class tortue
+{
+private:
+    /* data */
+    int posX = 0;
+    int posY = 0;
+public:
+    int couleur = 0;
+    tortue(/* args */);
+    ~tortue();
+    tortue(int x);
+    int getPosX();
+    int getPosY();
+    void setPosX(int x);
+    void setPosY(int y);
 };
 
-
-/*
-** Méthode: Constructeur de la classe
-*/
-tortue::tortue(COULEUR nouvCouleur){
-    this -> couleur= nouvCouleur;
-};
-
-/*
-    ** Méthode: Seteur de la valeur X
-    */
-void tortue::setCoordoX(int x)
+tortue::tortue(/* args */)
 {
-    this -> positionX= x;
 }
 
-
-/*
-    ** Méthode: Seteur de la valeur Y
-    */
-void tortue::setCoordoY(int y)
+tortue::~tortue()
 {
-    this -> positionY= y;
 }
 
-
-/*
-    ** Méthode: Accesseur à la valeur de X
-    */
-int tortue::getCoordoX()
-{
-    return this -> positionX ;
+int tortue::getPosX(){
+    return this->posX;
+}
+int tortue::getPosY(){
+    return this->posY;
+}
+void tortue::setPosX(int x){
+    this->posX = x;
+}
+void tortue::setPosY(int y){
+    this->posY = y;
 }
 
-
-/*
-    ** Méthode: Accesseur à la valeur de Y
-    */
-int tortue::getCoordoY()
-{
-    return this -> positionY;
+tortue::tortue(int x){
+    this->couleur = x;
 }
-
