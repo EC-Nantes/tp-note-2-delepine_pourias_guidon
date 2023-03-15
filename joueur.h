a569196a8f98005b6dd28d6b685eb800a0f018e0
Romain <101245889+Xquiratis@users.noreply.github.com>
2023-03-15 15:57:49 +0100

    code

--- joueur.h
+++ joueur.h
@@ -19,5 +19,31 @@ public:
     void Afficher();
     ~joueur();
 };
-
+joueur::joueur(COULEUR tuile)
+{
+    this->Tuile= tuile;
+}
+void joueur::Piocher(carte CartePioche)
+{
+    this->hand.push_back(CartePioche);
+}
+void joueur::Jouer()
+{
+    int NumCarte = 0;
+    //Step Choisir carte
+    this->Afficher();
+    std:cin >> NumCarte;
+    //Step 2
+}
+void joueur::Afficher()
+{
+    for(int i(0);i<this->hand.size();i++){
+        std::cout << "Carte "<<i << " : get COULEUR et get TYPE" << endl;
+        //std::cout <<  this->hand[0].getCouleur() << std::endl;
+    }
+    
+}
+joueur::~joueur()
+{
+}
 
