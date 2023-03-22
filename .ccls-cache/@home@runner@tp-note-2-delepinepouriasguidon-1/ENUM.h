2c5ebad36c8b299717fb4e5c96524247f93e0e56
Clément Guidon <101247576+clementguid@users.noreply.github.com>
2023-03-22 14:19:57 +0000

    ajouts commentaires

---
+++ .ccls-cache/@home@runner@tp-note-2-delepinepouriasguidon-1/ENUM.h
@@ -0,0 +1,18 @@
+#pragma once
+enum COULEUR {
+  INCOLOR = 0,
+  ROUGE = 1,
+  BLEU = 2,
+  VERT = 3,
+  JAUNE = 4,
+  VIOLET = 5
+};
+
+enum ACTION {
+  plusplus = 0,
+  plus1 = 1,
+  moins = 2,
+  fleche = 3,
+  flechefleche = 4,
+};
+
